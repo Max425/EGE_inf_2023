69c5602f7ebe12d1781db48ddbbb93ba09c77df5
Maxim Sikanov <sikanov.maksim@wb.ru>
2024-05-04 22:08:00 +0300

    Init project

---
+++ ege_2024/6/turtle_code.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+tracer(0) # чтобы сразу увидеть результат
+left(90) # поворачиваем в начальное положение
+
+# рисуем фигуру
+k = 40
+for i in range(7):
+    forward(10 * k)
+    right(120)
+
+# рисуем точки
+pu()
+for x in range(-k, k):
+    for y in range(-k, k):
+        goto(x * k, y * k)
+        dot(5)
+done() # чтобы рисунок не закрывался
